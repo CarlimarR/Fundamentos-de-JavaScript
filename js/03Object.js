40e384fe8fca399f787bb902d481228badbe2372
unknown <carlimar2002@gmail.com>
2025-11-22 16:55:34 -0400

    object

---
+++ js/03Object.js
@@ -0,0 +1,19 @@
+// object en javascript
+
+// definir objeto = va de (clave = valor)
+// puedo tener una clave que pueda ser otro objeto
+// elobjeto estara entre llaves
+
+const person = {
+    name: "Juan",
+    age: 25,
+    isStudent: true,
+    address: {
+        city: "Madrid",
+        country: "Spain"
+    },
+    hobbies: ["reading", "traveling", "sports"],
+    isStudent: true
+};
+
+console.log(person.name);
