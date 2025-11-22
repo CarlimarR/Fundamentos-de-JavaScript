84009fc4d7db7338338f1b98bb40cb99c98dd1f9
unknown <carlimar2002@gmail.com>
2025-11-22 16:24:23 -0400

    first commit

---
+++ js/01variables.js
@@ -0,0 +1,11 @@
+// Declaración de variables
+const name = "Juan";
+let age = 25;
+var lastName = "Perez";
+let isStudent = true;
+let price = 19.99;
+
+// donde esten (Const, let, var = quiere decir que se esta definiendo una variab;le en Java)
+
+// mostrar variable
+alert(name);
