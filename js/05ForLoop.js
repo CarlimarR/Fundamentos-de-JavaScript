ed66718ac56ba722f141968ab31c8fa3870f8cbe
unknown <carlimar2002@gmail.com>
2025-11-22 17:19:49 -0400

    for

---
+++ js/05ForLoop.js
@@ -0,0 +1,18 @@
+// bucle en javascript
+
+// for
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// recorrer un array
+// se define el arreglo
+const fruits = ["apple", "banana", "cherry"];
+
+// esto es la variable que se define para recorrer el array
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}   
+
+// el ++ es para incrementar el valor de i
