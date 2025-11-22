26cf3b82145d6213e81128682af6435a9813d680
unknown <carlimar2002@gmail.com>
2025-11-22 16:38:02 -0400

    array in javascript

---
+++ js/02Array.js
@@ -0,0 +1,14 @@
+// array en javascript
+fruits = ["apple", "banana", "cherry"];
+
+//console.log(fruits);
+
+fruits.push("orange");
+console.log(fruits);
+
+// me permite hubicar un objeto de la lista (logitud del array)
+console.log(fruits[1]);
+
+// me permite eliminar el ultimo elemento
+fruits.pop();
+console.log(fruits);
