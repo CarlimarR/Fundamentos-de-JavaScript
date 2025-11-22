b9386ddab940293ef251e706c300fa3b2bfb9183
unknown <carlimar2002@gmail.com>
2025-11-22 17:57:06 -0400

    methods

---
+++ js/07Methods.js
@@ -0,0 +1,29 @@
+// // metodos en javascript
+
+// // palabra reservada (this) funciona dentro de un objeto para referirse a si mismo
+// // es como una variable que se refiere al objeto en el que se encuentra
+// const person = {
+//     name: "Juan",
+//     lastName: "Perez",
+//     age: 25,
+//     isStudent: true,
+//     address: {
+//         city: "Madrid",
+//         country: "Spain"
+//     },
+//     hobbies: ["reading", "traveling", "sports"],
+//     isStudent: true,
+//     //metodo
+//     fullName: function() {
+//         return this.name + " " + this.lastName;
+//     }
+// };
+
+// //this es un atributo de este mismo objeto
+// // this funciona para llamar o traer un objeto
+
+// console.log(person.fullName());
+
+
+
+//calculadora utilizando atributos, metodos y this
