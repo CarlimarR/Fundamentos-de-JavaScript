3b59eda5c1960e2d3fea43d97089940e4a4113c4
unknown <carlimar2002@gmail.com>
2025-11-22 17:10:32 -0400

    conditionals

---
+++ js/04Conditionals.js
@@ -0,0 +1,14 @@
+// condicionales en javascript
+
+//evaluacion de edad para acceder al voto.
+const age = 18;
+
+if (age >= 18) {
+    console.log("Puedes votar");
+} else if (age < 18) {
+    console.log("No puedes votar");
+} else {
+    console.log("No puedes votar");
+}
+
+/// === esto es para hacer comparaciones extrictas con lo que se tiene, de esta manera el lenguuaje no hace suposiciones
